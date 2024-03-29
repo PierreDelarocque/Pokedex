385145aba6f6e61f9722a2e395e6e0f158d4865d
pierre delarocque <delarocque.pierre@gmail.com>
2024-03-29 15:39:42 +0100

    Map solution

--- my-react-app/src/components/NavBar.jsx
+++ my-react-app/src/components/NavBar.jsx
@@ -1,25 +1,18 @@
 import { PropTypes } from "prop-types";
 
-function Navbar({ pokemonIndex, setPokemonIndex, pokemonList }) {
-  const previousIndex = () => {
-    setPokemonIndex(pokemonIndex - 1);
-  };
-  const nextIndex = () => {
-    setPokemonIndex(pokemonIndex + 1);
-  };
-
+function Navbar({ setPokemonIndex, pokemonList }) {
   return (
     <div>
-      {pokemonIndex > 0 && <button onClick={previousIndex}>précédent</button>}
-      {pokemonIndex < pokemonList.length - 1 && (
-        <button onClick={nextIndex}>Suivant</button>
-      )}
+      {pokemonList.map((pokemon, index) => (
+        <button key={pokemon.name} onClick={() => setPokemonIndex(index)}>
+          {pokemon.name}
+        </button>
+      ))}
     </div>
   );
 }
 
 Navbar.propTypes = {
-  pokemonIndex: PropTypes.number.isRequiried,
   setPokemonIndex: PropTypes.func.isRequired,
   pokemonList: PropTypes.arrayOf(
     PropTypes.shape({ name: PropTypes.string.isRequired })
